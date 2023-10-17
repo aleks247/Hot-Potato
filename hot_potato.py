fac34d393409b05738595b5f211ef977c8a50fb9
Aleksandur Dimitrov <52452769+aleks247@users.noreply.github.com>
2023-10-17 22:26:53 +0300

    Update hot_potato.py

--- hot_potato.py
+++ hot_potato.py
@@ -1,9 +1,9 @@
 queue = input().split()
 n = int(input())
-
+index = 0
 while len(queue) > 1:
     # Използваме остатък от деление, за да определим детето, което изгаря
-    index = n % len(queue)
+    index = (index + n - 1) % len(queue)
     #Придава стойноста на removed_child, от първия елемент на List-a 'queue' и я премахва от него
     removed_child = queue.pop(index)
     print(f"Removed {removed_child}")
